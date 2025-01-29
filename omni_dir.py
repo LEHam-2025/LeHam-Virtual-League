dd3e2d26ec35371b171452b9c4f5998cc0ab8d83
A. Name <155173384+Anom-9@users.noreply.github.com>
2025-01-29 18:21:06 +0000

    commit omni_dir.py

--- omni_dir.py
+++ omni_dir.py
@@ -18,7 +18,7 @@ BIAS10 = 1.3 #MOTORS SO THEY TURN AT
 BIAS11 = 1.35	 #THE SAME SPEED
 
 
-def straight_1(distance):
+def straight_1(distance): #Moves using one set of wheels
     MOTOR1.motors[0].power = BIAS00 * DCONST
     MOTOR1.motors[1].power = BIAS01 * DCONST
     MOTOR2.motors[0].power = 0
@@ -29,7 +29,7 @@ def straight_1(distance):
     MOTOR2.motors[0].power = 0
     MOTOR2.motors[1].power = 0
 
-def straight_2(distance):
+def straight_2(distance): #Moves using the other set of wheels
     MOTOR2.motors[0].power = BIAS10 * DCONST
     MOTOR2.motors[1].power = BIAS11 * DCONST
     MOTOR1.motors[0].power = 0
@@ -40,7 +40,7 @@ def straight_2(distance):
     MOTOR2.motors[0].power = 0
     MOTOR2.motors[1].power = 0
 
-def cust_angle(angle, distance):
+def cust_angle(angle, distance): #Moves at a custom angle
     adjust1 = cos(radians(angle))
     adjust2 = sin(radians(angle))
 
