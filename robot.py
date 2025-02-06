b6225379032a1a86e404e8fb3e1b29cc88e104f4
A. Name <155173384+Anom-9@users.noreply.github.com>
2025-02-06 17:37:20 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -5,6 +5,4 @@ It only imports from logic
 
 from logic import *
 
-init_Zone() #This should be the complete function inside the logic module (name of funciton is to be decided later)
-while True:
-    pallet_place()
+game()
